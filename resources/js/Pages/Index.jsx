f7fb3aac3270738963e4cee56c8b4e114421d1bc
Z0KRI <david1166@live.com.mx>
2023-08-28 18:05:15 -0600

    Instalacion de Tailwind

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -2,7 +2,16 @@ import React, { useState } from 'react';
 
 const Test = () => {
     return (
-        <h1>This is test component</h1>
+        <div className="h-screen bg-gray-700 text-white flex justify-center items-center">
+            <div className="flex flex-col p-5">
+                <div className="flex justify-center text-2xl font-bold">
+                    Primera aplicacion
+                </div>
+                <div className="flex justify-center text-sm">
+                    React js + Laravel + Taildwind
+                </div>
+            </div>
+        </div>
     )
 }
 
